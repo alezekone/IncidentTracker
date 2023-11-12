e75dca4981c7f9ce4968f51f8ac74b9077cb083b
alezekone <alezek@gmail.com>
2023-11-12 12:37:24 -0300

    Commit inicial.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Incident Tracker
+
+Proyecto del Grupo N°2 para un sistema de seguimiento de incidentes.
---
+++ src/main/java/com/argprograma/etapa2/incidenttracker/IncidentTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.argprograma.etapa2.incidenttracker;
+
+/**
+ *
+ * @author Grupo 2
+ */
+public class IncidentTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
